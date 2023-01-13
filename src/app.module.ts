f1c3e02614478e47cfea1c0dd3f3b10a0b0af40e
Franklin David Tamayo Romo <ftamayor@dmq.quitodm.inst>
2023-01-13 16:17:49 -0500

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { EnviromentConfigModule } from './infraestructura/config/enviroment-config/enviroment-config.module';
 import { LoggerModule } from './infraestructura/logger/logger.module';
 import { ExceptionsModule } from './infraestructura/exceptions/exceptions.module';
+import { RepositoriesModule } from './infraestructura/repositories/repositories.module';
 
 @Module({
-  imports: [EnviromentConfigModule, TypeOrmModule, LoggerModule, ExceptionsModule],
+  imports: [EnviromentConfigModule, TypeOrmModule, LoggerModule, ExceptionsModule, RepositoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
